676cde1455340ba394ac95c1c6da710d56e86129
Vinicius Freitas <91697213+viniciusolzon@users.noreply.github.com>
2023-10-04 22:01:43 -0300

    construção arrumada

--- src/buscaLocal.cpp
+++ src/buscaLocal.cpp
@@ -11,8 +11,6 @@ bool CVRP::melhorou(double valor1, double valor2){
 int CVRP::calculaCustoSwap(Solucao *s, Data *d, int rota, int cliente1, int cliente2){
     int antes = 0, depois = 0, custo = 0;
 
-    // cout << "   Verificando custo de troca entre " << s->get_rotas()[rota][cliente1] << " e " << s->get_rotas()[rota][cliente2] << endl;
-
     if(cliente2 == cliente1 + 1){
         antes = d->get_custo(s->get_rotas()[rota][cliente1-1], s->get_rotas()[rota][cliente1]);
         antes+= d->get_custo(s->get_rotas()[rota][cliente1], s->get_rotas()[rota][cliente1+1]);
@@ -22,9 +20,8 @@ int CVRP::calculaCustoSwap(Solucao *s, Data *d, int rota, int cliente1, int clie
         depois = d->get_custo(s->get_rotas()[rota][cliente1-1], s->get_rotas()[rota][cliente2]);
         depois+= d->get_custo(s->get_rotas()[rota][cliente1], s->get_rotas()[rota][cliente2+1]);
         depois+= d->get_custo(s->get_rotas()[rota][cliente2], s->get_rotas()[rota][cliente1]);
-
-
     }
+
     else{
         antes = d->get_custo(s->get_rotas()[rota][cliente1-1], s->get_rotas()[rota][cliente1]);
         antes+= d->get_custo(s->get_rotas()[rota][cliente1], s->get_rotas()[rota][cliente1+1]);
@@ -38,8 +35,6 @@ int CVRP::calculaCustoSwap(Solucao *s, Data *d, int rota, int cliente1, int clie
         depois+= d->get_custo(s->get_rotas()[rota][cliente2-1], s->get_rotas()[rota][cliente1]);
         depois+= d->get_custo(s->get_rotas()[rota][cliente1], s->get_rotas()[rota][cliente2+1]);
     }
-    // cout << "   Custo antes = " << antes << endl;
-    // cout << "   Custo depois = " << depois << endl << endl;
 
     custo = depois - antes;
     return custo;
@@ -68,24 +63,12 @@ bool CVRP::melhorouSwap(Solucao *s, Data *d){
         // Se a rota escolhida tiver mais que 2 clientes precisamos testar todas combinações possíveis entre os clientes
         for(int i = 1; i < s->get_rotas()[rota_escolhida].size() - 2; i++){
             for(int j = i + 1; j < s->get_rotas()[rota_escolhida].size() - 1; j++){
-                // cout << "Custo antes da troca: " << s->get_custo() << endl;
                 custo = calculaCustoSwap(s, d, rota_escolhida, i, j);
-                // cout << "Custo depois da troca: " << s->get_custo() + custo << endl;
                 if(melhorou(melhor_custo, custo)){
-                    // cout << "Quanto vai adicionar no custo da rota assim q trocar = " << custo << endl;
                     melhor_custo = custo;
                     best_i = i;
                     best_j = j;
 
-                    // cout << endl;
-                    // for(int k = 0;k < s->get_rotas()[rota_escolhida].size() - 1; k++){
-                    //     cout << "Custo de " << s->get_rotas()[rota_escolhida][k] << " para " << s->get_rotas()[rota_escolhida][k+1];
-                    //     cout << " = " << d->get_custo(s->get_rotas()[rota_escolhida][k], s->get_rotas()[rota_escolhida][k+1]) << endl;
-                    // }
-
-
-                    // cout << endl << endl << "Melhor troca até agora: " << s->get_rotas()[rota_escolhida][best_i] << " com " << s->get_rotas()[rota_escolhida][best_j];
-                    // cout << endl << endl;
                 }
             }
         }
@@ -94,23 +77,6 @@ bool CVRP::melhorouSwap(Solucao *s, Data *d){
         if(melhor_custo < 0){
             cout << endl << "Vamos trocar: " << s->get_rotas()[rota_escolhida][best_i] << " com " << s->get_rotas()[rota_escolhida][best_j];
             cout << endl << endl;
-            
-            // cout << "Antes da troca\n";
-            // for(int t = 0; t < s->get_rotas()[rota_escolhida].size(); t++){
-            //     cout << s->get_rotas()[rota_escolhida][t] << " ";
-            // }
-            // cout << "\n";
-
-            // int aux = s->get_rotas()[rota_escolhida][best_i];
-            // s->atualizaRota(rota_escolhida, best_i, s->get_rotas()[rota_escolhida][best_j]);
-            // s->atualizaRota(rota_escolhida, best_j, aux);
-            // s->atualiza_custo(s->get_custo() + melhor_custo);
-
-            // cout << "Depois da troca\n";
-            // for(int t = 0; t < s->get_rotas()[rota_escolhida].size(); t++){
-            //     cout << s->get_rotas()[rota_escolhida][t] << " ";
-            // }
-            // cout << "\n";
             
             // Se houve alguma melhora em qualquer rota retornamos que houve uma melhora na solução
             houve_melhora = true;
@@ -125,14 +91,60 @@ int CVRP::calculaCustoShift(Solucao *s, Data *d, int rota1, int rota2, int clien
 
 bool CVRP::melhorouShift(Solucao *s, Data *d){
     
+    bool houve_melhora = false;
+
+    int qtd_rotas = s->get_rotas().size();
+    int tamanho_da_rota = 0;
+    // Precisamos de uma rota com no mínimo 2 clientes para tentar fazer a troca
+    for(int rota_escolhida = 0; rota_escolhida < qtd_rotas; rota_escolhida++){
+        // cout << "Verificando swap na rota: " << rota_escolhida+1 << endl;
+        tamanho_da_rota = s->get_rotas()[rota_escolhida].size();
+        
+        if(tamanho_da_rota < 4){
+            cout << "Rota muito pequena\n";
+            continue;
+        }
+
+        int melhor_custo = 0;
+        int custo = 0;
+        int best_i, best_j;
+
+        // Se a rota escolhida tiver mais que 2 clientes precisamos testar todas combinações possíveis entre os clientes
+        for(int i = 1; i < s->get_rotas()[rota_escolhida].size() - 2; i++){
+            for(int j = i + 1; j < s->get_rotas()[rota_escolhida].size() - 1; j++){
+                custo = calculaCustoSwap(s, d, rota_escolhida, i, j);
+                if(melhorou(melhor_custo, custo)){
+                    melhor_custo = custo;
+                    best_i = i;
+                    best_j = j;
+
+                }
+            }
+        }
+    
+        // Se o swap melhora a solucao, aplicamos o movimento na solucao atual
+        if(melhor_custo < 0){
+            cout << endl << "Vamos trocar: " << s->get_rotas()[rota_escolhida][best_i] << " com " << s->get_rotas()[rota_escolhida][best_j];
+            cout << endl << endl;
+            
+            // Se houve alguma melhora em qualquer rota retornamos que houve uma melhora na solução
+            houve_melhora = true;
+        }
+    }
+    return houve_melhora;
 }
 
 
+// RVND (Escolhe as estruturas de vizinhança de forma aleatória)
 void CVRP::BuscaLocal(Solucao *s, Data *d){
     if(melhorouSwap(s, d))
-        cout << "Houve melhora na busca local" << endl;
-    
+        cout << "Houve melhora no swap" << endl;
     else
-        cout << "Nao houve melhora na busca local" << endl << endl;
+        cout << "Nao houve melhora no swap" << endl;
+
+    if(melhorouShift(s, d))
+        cout << "Houve melhora no shift" << endl;
+    else
+        cout << "Nao houve melhora no shift" << endl;
 
 }
--- src/construcao.cpp
+++ src/construcao.cpp
@@ -6,69 +6,60 @@ Solucao CVRP::Construcao(Data *dados){
     // Constrói as rotas iniciais para cada veículo.
     int veiculoAtual = 1;
     int qtd_veiculos = dados->get_k();
+    int veiculo_usado = 0;
     for(int veiculoAtual = 1; veiculoAtual <= qtd_veiculos; veiculoAtual++){
-        cout << "Construindo rota para o veículo " << veiculoAtual << endl;
+        int veiculo_usado = 0;
+        // cout << "Construindo rota para o veículo " << veiculoAtual << endl;
         int capacidadeAtual = dados->get_Q();
         int atual = 0;
         
         while(capacidadeAtual > 0){
+            
             int proximo = vizinhoMaisProximo(atual, &solucao_atual, dados, capacidadeAtual);
-            cout << "   Cliente mais próximo do " << atual << " é " << proximo << " com custo " <<
-            dados->get_custo(atual, proximo) << " e demanda " <<  solucao_atual.get_clientes()[proximo- 1]->get_demanda() << endl;
+            // cout << "   Cliente mais próximo do " << atual << " é " << proximo << " com custo " <<
+            // dados->get_custo(atual, proximo) << " e demanda " <<  solucao_atual.get_clientes()[proximo- 1]->get_demanda() << endl;
+            
             if(proximo){
                 if (capacidadeAtual - solucao_atual.get_clientes()[proximo - 1]->get_demanda() >= 0){  // Verifica se a capacidade não é excedida
                     
                     solucao_atual.get_clientes()[proximo - 1]->set_atendido(true);
 
-                    cout << "   Capacidade atual do veículo: " << capacidadeAtual << endl;
+                    // cout << "   Capacidade atual do veículo: " << capacidadeAtual << endl;
                     capacidadeAtual -= solucao_atual.get_clientes()[proximo -1]->get_demanda();
                 
                     // Ao inserir o cliente na rota já podemos calcular o custo dele aqui
                     // Pra não precisar iterar sobre todos os clientes e calcular esse custo dps
                     solucao_atual.insereNaRota(veiculoAtual-1, proximo);
                     // Calcula o custo do cliente
-                    cout << "   Custo da solucao aumenta de  " << solucao_atual.get_custo();
-                    cout << " para " << solucao_atual.get_custo()+ dados->get_custo(atual, proximo) << endl << endl;
                     solucao_atual.atualiza_custo(solucao_atual.get_custo() + dados->get_custo(atual, proximo));
 
                     atual = proximo;
                 }
                 else{
-                    cout << "   Capacidade do veiculo atual excedida, retorna ao deposito\n\n";
+                    // cout << "   Capacidade do veiculo atual excedida, retorna ao deposito\n\n";
                     break;  // Se a capacidade for excedida, o veículo retorna ao depósito
                 }
             }
             else{
-                cout << "   Sem cliente proximo, o veiculo retorna ao deposito\n\n";
+                // cout << "   Sem cliente proximo, o veiculo retorna ao deposito\n\n";
                 break;  // Se não houver cliente próximo, o veículo retorna ao depósito
             }
         }
 
         // Coloca o 0 no final pq ele volta pro depósito
         solucao_atual.insereNaRota(veiculoAtual-1, 0);
-
-        cout << "   Ao voltar pro depósito o custo da solucao aumenta de  " << solucao_atual.get_custo();
-        cout << " para " << solucao_atual.get_custo()+ dados->get_custo(atual, 0) << endl << endl;
         solucao_atual.atualiza_custo(solucao_atual.get_custo() + dados->get_custo(atual, 0));
 
-        // Precisa disso?
-        // if(veiculoAtual+1 > dados->get_k()){
-        //     solucao_atual.get_rotas().push_back({});
-        // }
+        // Se a rota do veiculo atual possui mais de um vértice nela (já começa com o 0 que é o depósito)
+        if(solucao_atual.get_rotas()[veiculoAtual-1].size()>1)
+            veiculo_usado++;
+        // Soma o custo de uso de um veiculo
+        solucao_atual.atualiza_custo(solucao_atual.get_custo() + (veiculo_usado * dados->get_r()));
     }
     
     // Não precisa mais calcular o custo da rota aqui pq ele ta sendo calculado dentro da construção
     // calcularCustoTotal(&solucao_atual, *dados);
     return solucao_atual;
-}
-
-// Função para calcular a capacidade usada em uma rota.
-int CVRP::calcularCapacidadeUsada(vector<int> &rota, vector<Cliente*> clientes){
-    int capacidadeUsada = 0;
-    for (int clienteId : rota){
-        capacidadeUsada += clientes[clienteId]->get_demanda();
-    }
-    return capacidadeUsada;
 }
 
 // Função para encontrar o cliente mais próximo que ainda não foi atendido e que pode ser atendido pelo veículo atual.
@@ -89,7 +80,6 @@ int CVRP::vizinhoMaisProximo(int atual, Solucao *solucao, Data *dados, int capac
         // Verifica se o cliente atual ainda não foi atendido, se sua demanda pode
         // ser atendida e se o custo para atendê-lo é menor que o custo mínimo
         // encontrado até agora.
-
         if(!(solucao->get_clientes()[i-1]->get_atendido()) && solucao->get_clientes()[i-1]->get_demanda() <= capacidadeAtual && dados->get_custo(atual, i) < minCusto){
             minCusto = dados->get_custo(atual, i);
             mais_proximo = i;
@@ -99,31 +89,39 @@ int CVRP::vizinhoMaisProximo(int atual, Solucao *solucao, Data *dados, int capac
     return mais_proximo; // Retorna o índice do cliente mais próximo ou -1 se nenhum cliente puder ser atendido.
 }
 
+// Função para calcular a capacidade usada em uma rota.
+// int CVRP::calcularCapacidadeUsada(vector<int> &rota, vector<Cliente*> clientes){
+//     int capacidadeUsada = 0;
+//     for (int clienteId : rota){
+//         capacidadeUsada += clientes[clienteId]->get_demanda();
+//     }
+//     return capacidadeUsada;
+// }
+
 // Função para calcular o custo total da solução.
-// int calcularCustoTotal(vector<vector<int>> rotas, vector<Cliente> clientes, vector<vector<int>> &matrizCustos, int r){
-void CVRP::calcularCustoTotal(Solucao *s, Data d){
-    int custoTotal = 0;
-    int veiculosUsados = 0;
+// void CVRP::calcularCustoTotal(Solucao *s, Data d){
+//     int custoTotal = 0;
+//     int veiculosUsados = 0;
 
-    // Calcula o custo das rotas.
-    for (auto rota : s->get_rotas()){
-        if (!rota.empty()){
-            veiculosUsados++;
-            for (int i = 0; i < rota.size() - 1; i++){
-                custoTotal += d.get_custo(rota[i], rota[i+1]);
-            }
-        }
-    }
+//     // Calcula o custo das rotas.
+//     for (auto rota : s->get_rotas()){
+//         if (!rota.empty()){
+//             veiculosUsados++;
+//             for (int i = 0; i < rota.size() - 1; i++){
+//                 custoTotal += d.get_custo(rota[i], rota[i+1]);
+//             }
+//         }
+//     }
 
-    // Adiciona o custo fixo dos veículos utilizados.
-    custoTotal += d.get_r() * veiculosUsados;
+//     // Adiciona o custo fixo dos veículos utilizados.
+//     custoTotal += d.get_r() * veiculosUsados;
 
-    // Adiciona o custo de terceirização.
-    for (auto cliente : s->get_clientes()) {
-        if (!cliente->get_atendido()) {
-            custoTotal += cliente->get_custoTerceirizacao();
-        }
-    }
+//     // Adiciona o custo de terceirização.
+//     for (auto cliente : s->get_clientes()) {
+//         if (!cliente->get_atendido()) {
+//             custoTotal += cliente->get_custoTerceirizacao();
+//         }
+//     }
 
-    s->atualiza_custo(custoTotal);
-}
+//     s->atualiza_custo(custoTotal);
+// }
